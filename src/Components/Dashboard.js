aa91bf0ab5dbcfe5a91878a50b7ab8f2e77583ef
lrencealpas <mikelawrencealpas@gmail.com>
2023-11-20 17:22:35 +0800

    origin

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -97,25 +97,8 @@ function Dashboard({ onLogin }) {
 
         </div><br /><br /><br />
   </div>
-        
-        
-
-      {/* Logout confirmation box */}
-    {/* Footer */}
-
-    <footer className="footer">
-        <div className="footer-content">
-          <div className="footer-left">
-            <p className="copyright">Copyright © 2023 RESPO Inc. All rights reserved</p>
-          </div>
-          <div className="footer-right">
-            <p className="location">Cebu City, Philippines</p>
-          </div>
-        </div>
-    </footer>
     
-    
-    </div>
+  </div>  
   );
 }
 
