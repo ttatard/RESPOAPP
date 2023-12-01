5ec630daafb3d1df27655195ed4f31d20984c1fc
Asmodeus0613 <ayadekenneth07@gmail.com>
2023-12-01 12:35:05 +0800

    origin

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,5 +1,8 @@
+// LandingPage.js
+
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './CSS/LandingPage.css'; // Import the separated CSS file
 
 const LandingPage = () => {
   const handleScroll = () => {
@@ -9,28 +12,22 @@ const LandingPage = () => {
 
   return (
     <div className="landing-page">
-      <h1 style={{ color: 'red', textAlign: 'center', fontSize: '48px', fontWeight: 'bold' }}>
-        RESPO
-      </h1>
-      <div className="buttons" style={{ textAlign: 'center' }}>
+      <h1>WELCOME</h1>
+      <div className="buttons">
         <Link to="/login">
-          <button style={{ backgroundColor: 'white', color: 'black', marginRight: '10px' }}>
-            Login
-          </button>
+          <button>Login</button>
         </Link>
         <Link to="/signup">
-          <button style={{ backgroundColor: 'transparent', color: 'black', border: '1px solid black' }}>
-            SignUp
-          </button>
+          <button>SignUp</button>
         </Link>
       </div>
       {/* Implement the 'About Us' section here */}
-      <div className="about-us" style={{ display: 'none' }}>
+      <div className="about-us">
         <h2>About Us</h2>
         <p>Some information about your company or application...</p>
       </div>
       {/* Implement the scroll functionality to show 'About Us' section */}
-      <div className="scroll-down" style={{ textAlign: 'center', marginTop: '50px' }}>
+      <div className="scroll-down">
         <p>Scroll down to learn more</p>
         <i className="fa fa-angle-down" onClick={handleScroll}></i>
       </div>
