34afd2c86f57eab91c5f8c59a2bc537fad024e51
unknown <godwinsanjorjo24@gmail.com>
2023-12-13 18:42:59 +0800

    origin

--- src/Components/AdminRegister.js
+++ src/Components/AdminRegister.js
@@ -95,10 +95,8 @@ function AdminRegister() {
 
   return (
     <div className="form-container">
-      <h1>
-         <span style={{ color: 'black' }}>REGISTER YOUR </span>
-         <span style={{ color: 'red' }}>ACCOUNT</span>
-          </h1>
+      <h1> ADMIN <span className="red-text">ACCOUNT</span></h1>
+
       {showConfirmation && (
         <div className="confirmation-dialog">
           <p>Are you sure you want to sign up as admin?</p>
